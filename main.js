8dcffb66e717766f73e2563980be409eba35f2c7
RedGuy <I.maisuradze31.19@gmail.com>
2021-08-31 14:53:21 +0400

    js.st

---
+++ main.js
@@ -0,0 +1,29 @@
+const account1 = {
+  owner: "Irakli Maisuradze",
+  movements: [250, 400, -400, 3200, -700, -150, 100, 1500],
+  interestRate: 1.2, //%
+  pin: 3131,
+};
+
+const account2 = {
+  owner: "Ketevan Gulordava",
+  movements: [6000, 4000, -300, -800, -3200, -1000, 8500, -50],
+  interestRate: 1.5,
+  pin: 1717,
+};
+
+const account3 = {
+  owner: "Sandro Maisuradze",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3119,
+};
+
+const account4 = {
+  owner: "Jeremy Maisuradze",
+  movements: [430, 1000, 700, 50, 100],
+  interestRate: 1,
+  pin: 2020,
+};
+
+const accounts = [account1, account2, account3, account4];
